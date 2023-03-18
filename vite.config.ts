99c46f6ca82d8393d65e9cd4247d4b2ebecbb141
BryanS1989 <bryanstevenal@gmail.com>
2023-03-18 18:56:15 +0100

    Set base path for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+const path = require('path');
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    // base: path.resolve(__dirname, './dist'),
+    base: './',
     plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
