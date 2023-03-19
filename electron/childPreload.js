41c64ee6b211738bf3520076081215fd93ee9e6e
BryanS1989 <bryanstevenal@gmail.com>
2023-03-19 20:58:02 +0100

    Create childPreload and Preload

---
+++ electron/childPreload.js
@@ -0,0 +1,31 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    contextBridge.exposeInMainWorld('actionsChild', {
+        setClickedElement: (target) =>
+            ipcRenderer.send('actionsChild:setClickedElement', target),
+    });
+
+    var body = document.querySelector('body');
+    var script = document.createElement('script');
+    script.innerText =
+        "document.querySelector('body').addEventListener('click', (event) => {" +
+        'var eventData = {' +
+        '  tagName: event.target.tagName,' +
+        '  id: event.target.id,' +
+        '  className: event.target.className,' +
+        '  innerHTML: event.target.innerHTML,' +
+        '  innerText: event.target.innerText,' +
+        '  localName: event.target.localName,' +
+        '  nodeName: event.target.nodeName,' +
+        '  outerHTML: event.target.outerHTML,' +
+        '  outerText: event.target.outerText,' +
+        '  textContent: event.target.textContent,' +
+        '};' +
+        'console.log(event);window.actionsChild.setClickedElement({...eventData});' +
+        '});';
+
+    body.appendChild(script);
+});
--- electron/preload.js
+++ electron/preload.js
@@ -1,13 +1,12 @@
 // preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
 // All the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector);
-        if (element) element.innerText = text;
-    };
-
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency]);
-    }
+    contextBridge.exposeInMainWorld('actions', {
+        setUrl: (url) => ipcRenderer.send('actions:setUrl', url),
+        toggle: (url) => ipcRenderer.send('actions:toggle', url),
+        scrollDown: () => ipcRenderer.invoke('actions:scrollDown'),
+    });
 });
