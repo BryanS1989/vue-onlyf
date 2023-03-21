b8e127e4ae82f7410e9eb7f1b2bfbba581689adc
BryanS1989 <bryanstevenal@gmail.com>
2023-03-21 23:11:41 +0100

    Modify ChildPreload: - Create sendClickedElement - Add click event listener to all elements in child browserWindow - Create an empty preventAll function

--- electron/childPreload.js
+++ electron/childPreload.js
@@ -6,26 +6,30 @@ window.addEventListener('DOMContentLoaded', () => {
     contextBridge.exposeInMainWorld('actionsChild', {
         setClickedElement: (target) =>
             ipcRenderer.send('actionsChild:setClickedElement', target),
+        sendClickedElement: () =>
+            ipcRenderer.send('actionsChild:sendClickedElement'),
     });
 
     var body = document.querySelector('body');
     var script = document.createElement('script');
     script.innerText =
-        "document.querySelector('body').addEventListener('click', (event) => {" +
-        'var eventData = {' +
-        '  tagName: event.target.tagName,' +
-        '  id: event.target.id,' +
-        '  className: event.target.className,' +
-        '  innerHTML: event.target.innerHTML,' +
-        '  innerText: event.target.innerText,' +
-        '  localName: event.target.localName,' +
-        '  nodeName: event.target.nodeName,' +
-        '  outerHTML: event.target.outerHTML,' +
-        '  outerText: event.target.outerText,' +
-        '  textContent: event.target.textContent,' +
-        '};' +
-        'console.log(event);window.actionsChild.setClickedElement({...eventData});' +
-        '});';
-
+        'function preventAll() {' +
+        '}' +
+        "document.addEventListener('click', (event) => {" +
+        '    var eventData = {' +
+        '      tagName: event.target.tagName,' +
+        '      id: event.target.id,' +
+        '      className: event.target.className,' +
+        '      innerHTML: event.target.innerHTML,' +
+        '      innerText: event.target.innerText,' +
+        '      localName: event.target.localName,' +
+        '      nodeName: event.target.nodeName,' +
+        '      outerHTML: event.target.outerHTML,' +
+        '      outerText: event.target.outerText,' +
+        '      textContent: event.target.textContent,' +
+        '    };' +
+        '    console.log(event);window.actionsChild.setClickedElement({...eventData});' +
+        '    preventAll();' +
+        '}, true);';
     body.appendChild(script);
 });
